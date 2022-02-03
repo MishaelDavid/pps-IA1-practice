a716f0ffa460ab95395ffcbacbc23d596779ae28
Mishael David <97089344+MishaelDavid@users.noreply.github.com>
2022-02-03 08:57:48 +0000

    .

--- p1final.c
+++ p1final.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
-void 
+void input(int *a, int *b)
+{
+printf("enter two numbers\n");
+scanf("%d%d",a,b);
+}
+void add(int a, int b, int *sum)
+{
+  *sum = a+b; 
+}
+void output(int a, int b, int sum)
+{
+  printf("sum of %d + %d is %d\n",a,b,sum);
+}
+int main ()
+{
+  int a,b,c;
+input (&a,&b);
+add(a,b,&c);
+output(a,b,c);
+}
