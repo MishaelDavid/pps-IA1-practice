c4bfd03f171a27e25c8a3faf60bd11a6509349e4
Mishael David <97089344+MishaelDavid@users.noreply.github.com>
2022-02-03 10:43:59 +0000

    .

---
+++ p2final.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int input ()
+{
+  int a;
+printf("enter a number\n");
+scanf("%d", &a);
+return a;
+}
+int cmp(int a, int b, int c, int largest) 
+ {
+   if(a>b && a>c)
+  return 0;
+ else if (b>a && b>c)
+  return 1;
+ else
+  return 2;
+  }
+void output(int a, int b, int c, int largest)
+{
+  if(largest == 0)
+   printf("%d is the largest among %d,%d,%d",a,a,b,c);
+  else if(largest == 1)
+   printf("%d is the largest among %d,%d,%d",b,a,b,c);
+  else
+   printf("%d is the largest among %d,%d,%d",c,a,b,c);  
+}   
+int main()
+ {
+   int x,y,z,largest;
+   x = input();
+   y = input();
+   z = input();
+   largest = cmp(x,y,z,largest);
+   output(x,y,z,largest);
+   return 0;
+ }
---
+++ p4original.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int input()
+{
+  int a;
+  printf("enter the size of an array\n");
+  scanf("%d", &b[i]);
+}
